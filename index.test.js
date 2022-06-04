6756a3b8fa4e4e5d243859c1ad1c8c4f838a9032
Ernest <iendeavor.github.io@gmail.com>
2022-06-04 13:57:28 +0800

    test: fib(2) should call fib(1) and fib(0)

--- index.test.js
+++ index.test.js
@@ -1,4 +1,9 @@
-const { fib } = require('.')
+const fibModule = require('.')
+const { fib } = fibModule
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
 
 test('fib should be a function', () => {
   expect(typeof fib).toBe('function')
@@ -14,4 +19,15 @@ test('fib(1) should be 1', () => {
 
 test('fib(2) should be 1', () => {
   expect(fib(2)).toBe(1)
+})
+
+test('fib(2) should call fib(1) and fib(0)', () => {
+  const spyFib = jest.spyOn(fibModule, 'fib')
+
+  spyFib(2)
+
+  expect(spyFib).toHaveBeenCalledTimes(3)
+  expect(spyFib).toHaveBeenCalledWith(2)
+  expect(spyFib).toHaveBeenCalledWith(1)
+  expect(spyFib).toHaveBeenCalledWith(0)
 })
