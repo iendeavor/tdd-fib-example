a3d4cca12fe5f482b52abc4dc09f67d3b93d9ede
Ernest <iendeavor.github.io@gmail.com>
2022-06-04 13:58:28 +0800

    feat: fib(2) should call fib(1) and fib(0)

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const fib = (n) => {
   if (n === 0) return 0
   if (n === 1) return 1
-  return 0 + 1
+  return exports.fib(n - 1) + exports.fib(n - 2)
 }
 
-module.exports = {
-  fib
-}
+exports.fib = fib
